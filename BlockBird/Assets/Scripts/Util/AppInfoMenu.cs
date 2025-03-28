ae779cf123e10cc5d9f402362a06ed4d27cfabc1
creaturesoft <creaturesoft99@gmail.com>
2025-03-28 11:39:19 +0900

    Auto commit

--- BlockBird/Assets/Scripts/Util/AppInfoMenu.cs
+++ BlockBird/Assets/Scripts/Util/AppInfoMenu.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public static class AppInfoMenu
 {
-    [MenuItem("App Tools/Create AppInfo Asset")]
-    public static void CreateAppInfoAsset()
-    {
-        var asset = ScriptableObject.CreateInstance<AppInfoSO>();
-        AssetDatabase.CreateAsset(asset, "Assets/AppInfo.asset");
-        AssetDatabase.SaveAssets();
+    private const string assetPath = "Assets/AppInfo.asset";
 
+    [MenuItem("App Tools/AppInfo")]
+    public static void OpenOrCreateAppInfoAsset()
+    {
+        AppInfoSO asset = AssetDatabase.LoadAssetAtPath<AppInfoSO>(assetPath);
+
+        if (asset == null)
+        {
+            // 없으면 새로 생성
+            asset = ScriptableObject.CreateInstance<AppInfoSO>();
+            AssetDatabase.CreateAsset(asset, assetPath);
+            AssetDatabase.SaveAssets();
+            Debug.Log("AppInfo.asset 새로 생성됨");
+        }
+        else
+        {
+            Debug.Log("기존 AppInfo.asset 열기");
+        }
+
+        // 선택해서 인스펙터에서 바로 보이게
         EditorUtility.FocusProjectWindow();
         Selection.activeObject = asset;
     }
